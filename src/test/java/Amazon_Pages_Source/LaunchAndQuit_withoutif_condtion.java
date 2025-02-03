3ea7bec586af6c410dc7fe792d752a9a67931f33
Jayavibhav-Sali <jaysalimath61@gmail.com>
2025-02-03 12:56:09 +0530

    Update LaunchAndQuit_withoutif_condtion.java

--- src/test/java/Amazon_Pages_Source/LaunchAndQuit_withoutif_condtion.java
+++ src/test/java/Amazon_Pages_Source/LaunchAndQuit_withoutif_condtion.java
@@ -16,7 +16,7 @@ public class LaunchAndQuit_withoutif_condtion {
 	@BeforeMethod
 	public void launch() {
 		
-		driver= new FirefoxDriver();
+		driver= new ChromeDriver();
 		driver.get("https://www.amazon.in");
 	        driver.manage().window().maximize();
 		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(40));
